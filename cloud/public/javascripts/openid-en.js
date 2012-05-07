35c034a19c6ad6580bccb5409b2bd09a975776cc
simonduq <simonduq@afccc968-7a1d-4545-94b8-5b1ad357cb0c>
2012-05-07 11:21:45 +0000

    openid files

---
+++ cloud/public/javascripts/openid-en.js
@@ -0,0 +1,34 @@
+/*
+	Simple OpenID Plugin
+	http://code.google.com/p/openid-selector/
+	
+	This code is licensed under the New BSD License.
+*/
+
+var providers_large = {
+	google : {
+		name : 'Google',
+		url : 'https://www.google.com/accounts/o8/id'
+	},
+	aol : {
+		name : 'AOL',
+		label : 'Enter your AOL screenname:',
+		url : 'http://openid.aol.com/{username}'
+	},
+	myopenid : {
+		name : 'MyOpenID',
+		url : 'http://myopenid.com/'
+	},
+	openid : {
+		name : 'OpenID',
+		label : 'Enter your OpenID:',
+		url : null
+	}
+};
+
+var providers_small = null
+
+openid.locale = 'en';
+openid.sprite = 'en'; // reused in german& japan localization
+openid.signin_text = 'Sign-In';
+openid.image_title = 'log in with {provider}';
