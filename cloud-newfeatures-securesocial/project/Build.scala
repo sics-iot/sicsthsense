0a8e45f8051e6af21a1a068c5c15c473d8bd0226
beshr <beshr@afccc968-7a1d-4545-94b8-5b1ad357cb0c>
2013-01-29 15:58:12 +0000

    -Configuring SecureSocial. We still have to actually use it in the code :)
    PS: I created  google, twitter, linkedIn and GitHub accounts, 
    and I obtained developer keys to use their OAuth services
    sicsthsensebeshr@gmail.com
    and sicsthsense otherwise
    passwords:
    sicsthsense2013

--- cloud-newfeatures-securesocial/project/Build.scala
+++ cloud-newfeatures-securesocial/project/Build.scala
@@ -11,11 +11,13 @@ object ApplicationBuild extends Build {
       // Add your project dependencies here,
       "mysql" % "mysql-connector-java" % "5.1.18",
       "commons-io" % "commons-io" % "2.3",
-      "commons-codec" % "commons-codec" % "1.7"
+      "commons-codec" % "commons-codec" % "1.7",
+      "securesocial" % "securesocial_2.9.1" % "2.0.9"
     )
 
     val main = PlayProject(appName, appVersion, appDependencies, mainLang = JAVA).settings(
       // Add your own project settings here
+        resolvers += Resolver.url("SecureSocial Repository", url("http://securesocial.ws/repository/releases/"))(Resolver.ivyStylePatterns)
     )
 
 }
