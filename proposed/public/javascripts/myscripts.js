6b3c7dcc3d12466936e2718e0f99c2ce1be8b3c9
ljjm <ljjm@afccc968-7a1d-4545-94b8-5b1ad357cb0c>
2013-05-03 09:40:26 +0000

    Basic javascript validation on the resource name.
    just use alphanum() as an onclick javascript function.
    Should maybe also be done server side.

--- proposed/public/javascripts/myscripts.js
+++ proposed/public/javascripts/myscripts.js
@@ -601,5 +601,9 @@ fileRowClick = function(e) {
 
 //$("#vfileTree").bind("open_node.jstree select_node.jstree", browseFolder);
 
-
+function alphanum(e) {
+    var k;
+    document.all ? k = e.keyCode : k = e.which;
+    return ((k > 64 && k < 91) || (k > 96 && k < 123) || k == 8 || k == 32 || (k >= 48 && k <= 57));
+}
 
