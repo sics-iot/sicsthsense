97b969eddc1b73d0cd40f6d05fc162b871f5a09e
liamjjmcnamara <liamjjmcnamara@gmail.com>
2013-10-31 14:37:31 +0200

    add a config file install script

---
+++ installDBandConfig.sh
@@ -0,0 +1,6 @@
+# build the database
+cat buildDB.sql | mysql -u -root -p
+
+# move the default play config file for play, do not clobber
+cp -n web/conf/application.conf.default web/conf/application.conf
+
