5959516abe09de72469098d72257385635127fc0
liamjjmcnamara <liamjjmcnamara@gmail.com>
2014-01-24 17:23:25 +0100

    ...and the missing function files

---
+++ engine/src/main/java/com/sics/sicsthsense/core/functions/Function.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright (c) 2013, Swedish Institute of Computer Science
+ * All rights reserved.
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of The Swedish Institute of Computer Science nor the
+ *       names of its contributors may be used to endorse or promote products
+ *       derived from this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE SWEDISH INSTITUTE OF COMPUTER SCIENCE BE LIABLE 
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. */
+
+/* Description:
+ * TODO:
+ * */
+package com.sics.sicsthsense.core.functions;
+
+import java.util.List;
+import java.util.ArrayList;
+
+import com.sics.sicsthsense.core.*;
+import com.sics.sicsthsense.jdbi.*;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Function {
+	protected String type;
+	protected Logger logger;
+	protected StorageDAO storage;
+
+	public Function(String type) {
+		this.type = type;
+		this.storage = DAOFactory.getInstance();
+	}
+
+	public List<DataPoint> apply(List<Long> streamIds) {
+		List<DataPoint> rv = new ArrayList<DataPoint>();
+		return rv;
+	}
+
+	public String toString() {
+		return type;
+	}
+
+	public String getType()										{ return type; }
+	public void setType(String type)						{ this.type = type; }
+
+}
