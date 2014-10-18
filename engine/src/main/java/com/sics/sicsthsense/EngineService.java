f97257feb674fb8625f5c8c1d64201bcc1605522
Liam McNamara <liamjjmcnamara@gmail.com>
2014-10-18 15:01:26 +0200

    tab align

--- engine/src/main/java/com/sics/sicsthsense/EngineService.java
+++ engine/src/main/java/com/sics/sicsthsense/EngineService.java
@@ -11,11 +11,11 @@
  *     * Neither the name of The Swedish Institute of Computer Science nor the
  *       names of its contributors may be used to endorse or promote products
  *       derived from this software without specific prior written permission.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL THE SWEDISH INSTITUTE OF COMPUTER SCIENCE BE LIABLE 
+ * DISCLAIMED. IN NO EVENT SHALL THE SWEDISH INSTITUTE OF COMPUTER SCIENCE BE LIABLE
  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
  * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
  * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
@@ -78,13 +78,13 @@ public class EngineService extends Service<EngineConfiguration> {
 	@Override
 	public void initialize(Bootstrap<EngineConfiguration> bootstrap) {
 		bootstrap.setName("SicsthSense-Engine");
-    // Bundles
-    bootstrap.addBundle(new AssetsBundle("/assets/images", "/images"));
-    bootstrap.addBundle(new AssetsBundle("/assets/jquery", "/jquery"));
-    bootstrap.addBundle(new AssetsBundle("/assets/atmos", "/atmos"));
+		// Bundles
+		bootstrap.addBundle(new AssetsBundle("/assets/images", "/images"));
+		bootstrap.addBundle(new AssetsBundle("/assets/jquery", "/jquery"));
+		bootstrap.addBundle(new AssetsBundle("/assets/atmos", "/atmos"));
 		bootstrap.addBundle(new AssetsBundle("/assets/", "/"));
 
-    bootstrap.addBundle(new ViewBundle());
+		bootstrap.addBundle(new ViewBundle());
 		bootstrap.addBundle(new DBIExceptionsBundle());
 	}
 
@@ -133,7 +133,7 @@ public class EngineService extends Service<EngineConfiguration> {
     // Attach Atmosphere servlet
 		addServlet(environment);
 
-    // Session handler to enable automatic session handling 
+    // Session handler to enable automatic session handling
     environment.setSessionHandler(new SessionHandler());
 	}
 
