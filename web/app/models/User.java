569b52d6f2f2e88fa1fc7f33d230b13f10f15877
Liam McNamara <liamjjmcnamara@gmail.com>
2014-10-18 17:37:33 +0200

    another debug

--- web/app/models/User.java
+++ web/app/models/User.java
@@ -205,7 +205,7 @@ public class User extends Model implements Comparable<User> { //PathBindable<Use
 		if (user==null) {Logger.warn("User is null");}
 		if (user.token!=null) {Logger.warn("Token is null");}
 		if (token.equals(user.token)) {Logger.warn("Token doesnt match");}
-		if (this.id == user.id) {Logger.warn("id doesnt match");}
+		if (this.id != user.id) {Logger.warn("id doesnt match: "+this.id+" "+user.id);}
 
 		//return user!=null && user.token!=null && token.equals(user.token) && this.id == user.id;
 		return user!=null  && this.id == user.id;
