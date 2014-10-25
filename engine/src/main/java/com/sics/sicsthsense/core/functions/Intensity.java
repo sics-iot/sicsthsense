9c647f5dfa908e6ef44c8a81471132d7252143ca
Liam McNamara <liamjjmcnamara@gmail.com>
2014-10-25 16:57:36 +0200

    full 9,8 gravity

--- engine/src/main/java/com/sics/sicsthsense/core/functions/Intensity.java
+++ engine/src/main/java/com/sics/sicsthsense/core/functions/Intensity.java
@@ -61,7 +61,7 @@ public class Intensity extends Function {
 	  if (X==null || Y==null || Z==null) {throw new Exception("No acceleration data");}
 	  for (int c=0; c<X.size(); ++c) {
 		double magnitude = magnitude3D(X.get(c).getValue(), Y.get(c).getValue(), Z.get(c).getValue());
-		double gravity = 3;//9.8;
+		double gravity = 9.8;//9.8;
 		logger.info("magnitude: "+magnitude);
 		magnitude -= gravity;
 		logger.info("magnitude - gravity: "+magnitude);
