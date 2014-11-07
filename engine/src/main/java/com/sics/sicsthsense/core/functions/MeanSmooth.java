23e7e44379fa7521270f18ca4e8ab83f5284257a
Liam McNamara <liamjjmcnamara@gmail.com>
2014-11-07 17:18:57 +0100

    alt smoother

---
+++ engine/src/main/java/com/sics/sicsthsense/core/functions/MeanSmooth.java
@@ -0,0 +1,80 @@
+/*
+ * Copyright (c) 2013, Swedish Institute of Computer Science
+ * All rights reserved.
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of The Swedish Institute of Computer Science nor the
+ *       names of its contributors may be used to endorse or promote products
+ *       derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE SWEDISH INSTITUTE OF COMPUTER SCIENCE BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. */
+
+/* Description: Smoothing function - takes mean of previous N points of one stream
+ * TODO:
+ * */
+package se.sics.sicsthsense.core.functions;
+
+import java.util.List;
+import java.util.ArrayList;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import se.sics.sicsthsense.core.*;
+import se.sics.sicsthsense.jdbi.*;
+
+public class MeanSmooth extends Function {
+	private final Logger logger = LoggerFactory.getLogger(MeanSmooth.class);
+	public int N;
+
+	public MeanSmooth(StorageDAO storage) {
+		super(storage,"meansmooth");
+		this.type = "meansmooth";
+		this.N = 10;
+	}
+
+	public List<DataPoint> apply(List<Long> streamIds) {
+		long latest=-1;
+		List<DataPoint> rv = new ArrayList<DataPoint>();
+
+		if (streamIds==null) { logger.error("Stream IDs are null!!"); return rv; }
+		if (streamIds.size()!=1) { logger.error("Stream antecedants size 1!= "+streamIds.size()); return rv; }
+
+		List<DataPoint> dps = storage.findPointsByStreamId(streamIds.get(0),N);
+		int count=dps.size(); // we may have less than
+		double acc = 0;
+		//logger.info("Acc: "+acc);
+		for (int c=dps.size()-1; c>=0; --c) {
+			DataPoint dp = dps.get(c);
+			logger.info("dp: "+dp.toString());
+			logger.info("Value: "+dp.getValue());
+			acc += dp.getValue();;
+			logger.info("Acc: "+acc);
+			if (dp.getTimestamp() > latest) {latest = dp.getTimestamp();}
+		}
+		if (count>0) { //check we have some data points
+			rv.add(new DataPoint(latest, (acc/count)));
+		} else {
+			logger.warn("No points to smooth!");
+		}
+		return rv;
+	}
+
+	public String toString() {
+		return type;
+	}
+
+}
